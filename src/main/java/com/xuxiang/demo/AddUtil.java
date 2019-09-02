fce752c3d0bc37e4cea6d99bde301dc84e70d203
insistvictory <1258226626@qq.com>
2019-09-02 23:34:20 +0800

    init

---
+++ src/main/java/com/xuxiang/demo/AddUtil.java
@@ -0,0 +1,10 @@
+package com.xuxiang.demo;
+
+/**
+ * Created by Administrator on 2019/9/2.
+ */
+public class AddUtil {
+    public int add(int a , int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/com/xuxiang/demo/AddUtilTest.java
@@ -0,0 +1,15 @@
+package com.xuxiang.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2019/9/2.
+ */
+public class AddUtilTest {
+    @Test
+    public void add() throws Exception {
+    }
+
+}
