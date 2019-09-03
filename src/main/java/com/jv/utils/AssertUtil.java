82f3d46f9399819b6d9c8d0ce2f66cba12b2dffa
insistvictory <1258226626@qq.com>
2019-09-03 21:34:38 +0800

    添加工具类

---
+++ src/main/java/com/jv/utils/AssertUtil.java
@@ -0,0 +1,43 @@
+package com.jv.utils;
+
+public class AssertUtil {
+    public static void nonull(Object o) {
+        nonull(o, "the object must not be null");
+    }
+
+    /**
+     * Asserts that the given object is not {@code null}.
+     *
+     * @param o The object to check.
+     * @param message A message text which will be used as message of the resulting exception if the given object is
+     * {@code null}.
+     * @throws IllegalArgumentException In case the given object is {@code null}.
+     */
+    public static <T> T nonull(T o, String message) {
+        if (o == null) {
+            throw new IllegalArgumentException(message);
+        }
+
+        return o;
+    }
+
+    public static <T> T nonull(final T object, final String message, final Object... values) {
+        return nonull(object, String.format(message, values));
+    }
+
+    public static void yes(boolean condition, String message) {
+        if (!condition) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void yes(final boolean expression, final String message, final Object... values) {
+        yes(expression, String.format(message, values));
+    }
+
+    public static void notEmpty(String s, String message) {
+        if (s.length() == 0) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
---
+++ src/main/java/com/jv/utils/FileUtil.java
@@ -0,0 +1,23 @@
+package com.jv.utils;
+
+public class FileUtil {
+    public static String extension(String filename) {
+        if ((filename != null) && (filename.length() > 0)) {
+            int dot = filename.lastIndexOf('.');
+            if ((dot >-1) && (dot < (filename.length() - 1))) {
+                return filename.substring(dot + 1);
+            }
+        }
+        return filename;
+    }
+
+    public static String trimExtension (String filename) {
+        if ((filename != null) && (filename.length() > 0)) {
+            int dot = filename.lastIndexOf('.');
+            if ((dot >-1) && (dot < (filename.length()))) {
+                return filename.substring(0, dot);
+            }
+        }
+        return filename;
+    }
+}
